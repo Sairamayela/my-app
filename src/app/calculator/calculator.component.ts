e2afe1f3d7ef4a20250cf3a2ec7817a9bb37138c
sairamayela <sairamayela@gmail.com>
2023-02-23 12:22:23 +0530

    calculator implemanted

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -5,11 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
-export class CalculatorComponent implements OnInit {
+export class CalculatorComponent {
+public number1:number =0;
+public number2:number =0;
+public result:number =0;
+  
+add(){
+  this.result = this.number1 + this.number2;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+}
+sub(){
+  this.result = this.number1 - this.number2;
+}
+mul(){
+  this.result = this.number1 * this.number2;
+}
+div(){
+  this.result = this.number1 / this.number2;
+}
 
 }
